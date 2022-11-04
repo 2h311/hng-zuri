1f945fe7915e193dedacff7ce01f8f85b3ba250c
2h311 <kcodepal@outlook.com>
2022-11-04 10:31:15 -0500

    made some adjustments

--- main.py
+++ main.py
@@ -6,21 +6,23 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
-
 class OperationType(Enum):
 	ADDITION = "addition"
 	SUBTRACTION ="subtraction"
 	MULTIPLICATION = "multiplication"
 
-class RequestBody(BaseModel):
+
+class RequestBodyModel(BaseModel):
 	x: int | None = None
 	y: int | None = None
 	operation_type: OperationType
 
-class ResponseBody(BaseModel):
+
+class ResponseBodyModel(BaseModel):
 	slackUsername: str = "olakanmi"
 	operation_type: OperationType
 	result: int
+
 
 operation_dict = {
 	OperationType.ADDITION: add,
@@ -28,26 +30,29 @@ operation_dict = {
 	OperationType.MULTIPLICATION: mul
 }
 
+
 app = FastAPI()
-app.add_middleware(
-	CORSMiddleware,
-	allow_origins=["*"],
-	allow_credentials=["*"],
-	allow_methods=["*"],
-	allow_headers=["*"],
-)
+# app.add_middleware(
+# 	CORSMiddleware,
+# 	allow_origins=["*"],
+# 	allow_credentials=["*"],
+# 	allow_methods=["*"],
+# 	allow_headers=["*"],
+# )
+
 
 @app.get("/")
 def index():
 	return { "slackUsername": "olakanmi", "backend": True, "age": 25, "bio": "hello world. I love chihuahuas" }
 
 
-@app.post("/", response_model=ResponseBody)
-async def create_result(request_body: RequestBody):
+@app.post("/")
+async def create_result(request_body: RequestBodyModel):
+
 	x, y = request_body.x, request_body.y
 	operation_type = request_body.operation_type
 	operation_type_value = operation_type.value 
 	
 	func = operation_dict[operation_type]
 	result = func(x, y)
-	return {"result": result, "operation_type": operation_type_value, }
+	return {"slackUsername": "olakanmi", "result": result, "operation_type": operation_type_value, }	
